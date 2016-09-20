cdae6e13c8f22091d479d321e98ed33aeaaf3f00
Andrew Gnott <agnott@nd.edu>
2016-09-19 20:30:58 -0400

    Unupdate

--- run.sh
+++
@@ -1,3 +0,0 @@
-rm -f main.js
-find assets/config assets/geography assets/language assets/population assets/science assets/utilities assets/main.js | grep '.js' | while read file; do cat $file >> main.js; done;
-http-server
